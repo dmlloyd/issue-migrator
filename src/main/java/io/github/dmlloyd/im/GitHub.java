1240c6ae4915e18b5704fd0cf79acec4b85caf54
David M. Lloyd <david.lloyd@redhat.com>
2024-12-04 12:57:46 -0600

    Add basic comment request construction
    Fixes #10

--- src/main/java/io/github/dmlloyd/im/GitHub.java
+++ src/main/java/io/github/dmlloyd/im/GitHub.java
@@ -10,6 +10,7 @@ import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Stuff for dealing with GitHub.
@@ -32,11 +33,10 @@ public final class GitHub {
         public JsonObject issueCreateRequest(Issue issue) {
             JsonObjectBuilder root = Json.createObjectBuilder();
             root.add("title", issue.summary());
-            // todo: transform issue links
             // todo: transform user references
             StringBuilder mdDesc = new StringBuilder(1024);
             mdDesc.append("This issue was imported from JIRA. The original issue was: ");
-            mdDesc.append('[').append(issue.key()).append(']').append('(').append(jiraUrl).append(')');
+            mdDesc.append('[').append(issue.key()).append(']').append('(').append(jiraUrl).append("browse/").append(issue.key()).append(')').append("\n\n");
             mdDesc.append(FlexmarkHtmlConverter.builder().build().convert(issue.description()));
             mdDesc.append("\n\nOriginal issue creation date: ").append(issue.created());
             mdDesc.append("\nOriginal issue updated date: ").append(issue.updated());
@@ -47,7 +47,32 @@ public final class GitHub {
             // todo: milestone
             // todo: labels
             // todo: assignees (array)
-            // todo: comments
+            return root.build();
+        }
+    }
+
+    /**
+     * Used to construct comment creation request.
+     */
+    public static final class CommentCreationFactory {
+        private final URI jiraUrl;
+        private final Map<String, String> userMapping;
+        private final Map<String, Integer> mappedIssueNumbers;
+
+        public CommentCreationFactory(final URI jiraUrl, final Map<String, String> userMapping, final Map<String, Integer> mappedIssueNumbers) {
+            this.jiraUrl = jiraUrl;
+            this.userMapping = userMapping;
+            this.mappedIssueNumbers = mappedIssueNumbers;
+        }
+
+        public JsonObject commentCreateRequest(Issue issue, Comment comment) {
+            JsonObjectBuilder root = Json.createObjectBuilder();
+            StringBuilder mdDesc = new StringBuilder(1024);
+            mdDesc.append("This comment was imported from JIRA.\n\n");
+            // todo: comment link
+            mdDesc.append(FlexmarkHtmlConverter.builder().build().convert(comment.body()));
+            mdDesc.append("\n\nOriginal comment creation date: ").append(comment.created());
+            root.add("body", remapIssueKeys(mdDesc.toString(), mappedIssueNumbers));
             return root.build();
         }
     }
@@ -71,26 +96,31 @@ public final class GitHub {
                 throw new IllegalArgumentException("The issue key " + issue.key() + " was not mapped");
             }
             // this is pretty janky
-            StringBuilder newBody = new StringBuilder(originalBody.length());
-            Pattern pattern = Pattern.compile("(?:https?://[a-zA-Z0-9./]+)?([A-Z0-9]+-\\d+)(?:\\\\?\\S+)?");
-            Matcher matcher = pattern.matcher(originalBody);
-            while (matcher.find()) {
-                String key = matcher.group(1);
-                Integer id = mappedIssueNumbers.get(key);
-                if (id != null) {
-                    matcher.appendReplacement(newBody, "#" + id);
-                } else {
-                    // didn't match, just leave it alone
-                    matcher.appendReplacement(newBody, matcher.group(0));
-                }
-            }
-            matcher.appendTail(newBody);
-            root.add("body", newBody.toString());
+            root.add("body", remapIssueKeys(originalBody, mappedIssueNumbers));
             // todo: issue status
             // root.add("state", "open"|"closed");
             // root.add("state_reason", "completed"|"not_planned"|"reopened");
             return root.build();
         }
+    }
+
+    private static @NotNull String remapIssueKeys(final String originalBody, final Map<String, Integer> mappedIssueNumbers) {
+        StringBuilder newBody = new StringBuilder(originalBody.length());
+        Pattern pattern = Pattern.compile("(?:https?://[a-zA-Z0-9./]+)?([A-Z0-9]+-\\d+)(?:\\\\?\\S+)?");
+        Matcher matcher = pattern.matcher(originalBody);
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            Integer id = mappedIssueNumbers.get(key);
+            if (id != null) {
+                matcher.appendReplacement(newBody, "#" + id);
+            } else {
+                // didn't match, just leave it alone
+                matcher.appendReplacement(newBody, matcher.group(0));
+            }
+        }
+        matcher.appendTail(newBody);
+        String body = newBody.toString();
+        return body;
     }
 
     private static URI fixJiraUri(final URI jiraUrl) {
